cc144a77f046c999909c5afb9f4630410f8bb2d5
Vamshi Krishna <vamshi973133@gmail.com>
2026-02-24 16:04:49 +0530

    notifications and arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,19 +6,23 @@ import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
 import commentRoutes from "./routes/comment.route.js"
-
+import notificationRoutes from "./routes/notification.route.js"
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+app.use(clerkMiddleware())
+app.use(arcjetMiddleware)
 app.get('/',(req,res)=>{
     res.send("Hello from server")
 })
 app.use("/api/users",userRoutes)
 app.use("/api/post",postRoutes)
 app.use("api/comments",commentRoutes)
+app.use("/api/notifications",notificationRoutes)
 
 //error handling middleware
 app.use((err,req,res,next)=>{
