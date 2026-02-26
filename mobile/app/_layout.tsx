6c9bb612f56a4224ed844fe5a199164e8bf0b324
Vamshi Krishna <vamshi973133@gmail.com>
2026-02-26 10:08:48 +0530

    auth implemented

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,15 @@
 import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
 import "../global.css"
 
 export default function RootLayout() {
-  return <Stack />;
+  return( 
+  <ClerkProvider tokenCache={tokenCache}>
+  <Stack>
+    <Stack.Screen name="(auth)" options={{headerShown:false,headerTitleAlign:'center'}}/>
+  </Stack>
+</ClerkProvider>
+)
 }
---
+++ mobile/hooks/useSocialAuth.ts
@@ -0,0 +1,28 @@
+import { useSSO } from "@clerk/clerk-expo"
+import { useState } from "react"
+import { Alert } from "react-native";
+
+
+export const useSocialAuth = () => {
+ const [isLoading,setIsLoading] = useState(false)
+ const {startSSOFlow} = useSSO();
+
+ const handelSocialAuth = async(strategy:"oauth_google"| "oauth_apple")=>{
+  setIsLoading(true)
+  try {
+    const { createdSessionId,setActive } = await startSSOFlow({strategy})
+    if(createdSessionId && setActive){
+        await setActive({session:createdSessionId});
+    }
+  } catch (err) {
+    console.log(err);
+    const provider = strategy === "oauth_google"?"Google":"Apple"
+    Alert.alert("Error",`Failed to sign in with ${provider}. please try again `)
+    
+  }finally{
+    setIsLoading(false)
+  }
+ }
+
+    return {isLoading,handelSocialAuth}
+} 
